c818ec166dc0786703c7cc52e6907fb5808643b5
omelasN <semeteeva.n@gmail.com>
2023-11-23 16:51:37 +0600

    Created registration/authentication functional for Users

---
+++ users/models.py
@@ -0,0 +1,41 @@
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.db import models
+from rest_framework_simplejwt.tokens import RefreshToken
+
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, username, email, password=None):
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, username, email, password=None):
+        user = self.create_user(username=username, email=email, password=password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save()
+        return user
+
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True, max_length=255)
+    username = models.CharField(max_length=255, unique=True)
+    first_name = models.CharField(max_length=255, blank=True)
+    last_name = models.CharField(max_length=255, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+
+    objects = UserManager()
+    REQUIRED_FIELDS = ['email']
+
+    def tokens(self):
+        refresh = RefreshToken.for_user(self)
+        return {
+            'refresh': str(refresh),
+            'access': str(refresh.access_token)
+        }
+
+    def str(self):
+        return f"{self.username}"
---
+++ users/serializers.py
@@ -0,0 +1,44 @@
+from rest_framework import serializers
+from .models import User
+from django.contrib import auth
+
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, max_length=15, min_length=8)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
+    def save(self):
+        user = User(
+            username=self.validated_data['username'],
+            email=self.validated_data['email'])
+        password = self.validated_data['password']
+
+        if not user.username.isalnum():
+            raise serializers.ValidationError("Username must consist of letters and numbers only.")
+
+        user.set_password(password)
+        user.save()
+
+
+class LoginSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(max_length=255)
+    password = serializers.CharField(max_length=15, min_length=8, write_only=True)
+    tokens = serializers.SerializerMethodField
+
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'tokens']
+
+    def validate(self, attrs):
+        username = attrs.get('username', '')
+        password = attrs.get('password', '')
+
+        user = auth.authenticate(username=username, password=password)
+
+        return {
+            'username': user.username,
+            'tokens': user.tokens()
+        }
